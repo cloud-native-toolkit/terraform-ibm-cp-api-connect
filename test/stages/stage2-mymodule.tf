c4f80ea00bc98e36877052243b9b88759e13af5c
Andrew Trice <amtrice@us.ibm.com>
2021-07-27 16:27:03 -0400

    added vars for module testing
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage2-mymodule.tf
+++ test/stages/stage2-mymodule.tf
@@ -1,3 +1,10 @@
 module "dev_tools_mymodule" {
   source = "./module"
+
+  catalog_name        = module.cp4i.name
+  cluster_type        = module.dev_cluster.type_code
+  cluster_config_file = module.dev_cluster.config_file_path
+  namespace           = module.dev_tools_namespace.name
+  platform-navigator-name = module.platform-navigator.name
+  dashboard           = false
 }
